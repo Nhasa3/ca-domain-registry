51fbbf19134e2a04b73e81fc2664f5fb85f2f3f5
Nesar <nhasa0011@gmail.com>
2026-04-30 22:59:52 -0400

    Created the base project

---
+++ Domain_Registery/src/main/java/com/CIRA_N/Domain_Registery/DomainRegisteryApplication.java
@@ -0,0 +1,13 @@
+package com.CIRA_N.Domain_Registery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainRegisteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainRegisteryApplication.class, args);
+	}
+
+}
