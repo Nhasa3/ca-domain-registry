aebe46a21d7e50e652be4f2b7647d657ae0382ba
Nesar <nhasa0011@gmail.com>
2026-04-30 23:12:44 -0400

    added dependencies and controller directory with HomeController file

---
+++ Domain_Registery/src/main/java/com/CIRA_N/Domain_Registery/Controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.CIRA_N.Domain_Registery.Controller;
+
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class HomeController {
+
+    public String index(){
+
+        return "index";
+    }
+
+}
