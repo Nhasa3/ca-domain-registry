119ed86329e48c7a50fcd602f5815a6929697a2b
Nesar <nhasa0011@gmail.com>
2026-05-06 13:29:36 -0400

    Implemented domain search in homepage and will handles situations where there the domain is taken or not

--- Domain_Registery/src/main/java/com/CIRA_N/Domain_Registery/Controller/DomainController.java
+++ Domain_Registery/src/main/java/com/CIRA_N/Domain_Registery/Controller/DomainController.java
@@ -1,11 +1,15 @@
 package com.CIRA_N.Domain_Registery.Controller;
 
+import ch.qos.logback.core.model.Model;
 import com.CIRA_N.Domain_Registery.Services.DomainService;
 import com.CIRA_N.Domain_Registery.Services.UserService;
+import com.CIRA_N.Domain_Registery.model.Domain;
 import com.CIRA_N.Domain_Registery.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 
@@ -24,7 +28,28 @@ public class DomainController {
         return userService.getUserByEmail(principal.getName());
     }
 
-    
+    // Check Domain availability (from homepage search)
+    @GetMapping("/search")
+    public String searchDomain(
+            @RequestParam String name,
+            Model model
+    ){
+        String domainName = name.toLowerCase().trim();
+        boolean available = domainService.isAvailable(domainName);
 
+        model.addAttribute("domainName", domainName);
+        model.addAttribute("available", available);
+
+        // If taken, show the domain details
+        if(!available){
+            try{
+                Domain domain = domainService.getDomainByName(domainName);
+                model.addAttribute("domain", domain);
+            }catch (Exception ignored) {}
+        }
+        return "search-result";  // Loads templates/serach-result.html
+    }
+
+    
 
 }
