6fdfa73487d3213b3d81e255a1890de8292acfc7
Nesar <nhasa0011@gmail.com>
2026-05-06 13:13:22 -0400

    Added authintication controller which give functionality to register the user and login

--- Domain_Registery/src/main/java/com/CIRA_N/Domain_Registery/Controller/HomeController.java
+++ Domain_Registery/src/main/java/com/CIRA_N/Domain_Registery/Controller/HomeController.java
@@ -1,14 +1,19 @@
 package com.CIRA_N.Domain_Registery.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class HomeController {
 
+    @GetMapping("/")
     public String index(){
-
         return "index";
     }
 
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
 }
